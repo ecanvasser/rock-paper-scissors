6912239867bde22b88d8e9b7420eab0763520e15
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-28 15:20:25 -0700

    Route event listeners to buttons

--- main.js
+++ main.js
@@ -9,43 +9,62 @@ function playRound(playerSelection, computerSelection) {
     let playerDisplayValue = playerSelection[0].toUpperCase() + playerSelection.slice(1,);
 
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        return 'You lost this round! Paper beats rock'
+        return 'You lost this round! Paper beats rock';
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor') {
-        return 'You lost this round! Scissors beat paper'
+        return 'You lost this round! Scissors beat paper';
     } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'rock') {
-        return 'You lost this round! Rock beats scissor'
+        return 'You lost this round! Rock beats scissor';
     } else if (playerSelection.toLowerCase() === computerSelection) {
-        return 'Tie!'
+        return 'Tie!';
     } else {
-        return `Winner winner chicken dinner! ${playerDisplayValue} beats ${computerSelection}`
+        return `Winner winner chicken dinner! ${playerDisplayValue} beats ${computerSelection}`;
     }
 }
 
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(prompt('Rock, paper, or scissor?'), computerPlay());
-        
-        if (result.includes('win')) {
-            alert(`Round ${i + 1}: ` + result);
-            userScore++;
-        } else if (result.includes('Tie')) {
-            alert(`Round ${i + 1}: ` + result);
-            userScore += 0;
-            computerScore += 0;
-        } else {
-            alert(`Round ${i + 1}: ` + result);
-            computerScore++;
-        }
-    }
-
-    if (userScore > computerScore) {
-        return alert('Congrats! You win');
-    } else if (userScore === computerScore) {
-        return alert('It\'s a tie!');
+function game(choice) {
+    
+    let result = playRound(choice, computerPlay());
+    
+    if (result.includes('win')) {
+        alert(result);
+        +userScore++
+        console.log(userScore)
+    } else if (result.includes('Tie')) {
+        alert(result);
+        console.log(userScore, computerScore)
     } else {
+        alert(result);
+        +computerScore++
+        console.log(computerScore)
+    };
+
+    if (userScore == 5) {
+        userScore = 0;
+        computerScore = 0;
+        return alert('Congrats! You win');
+    } else if (computerScore == 5) {
+        userScore = 0;
+        computerScore = 0;
         return alert('Rats! You lost');
     }
-}
+};
+
+let userScore = document.querySelector('.playerScore').textContent;
+userScore = 0;
+let computerScore = document.querySelector('.compScore').textContent;
+computerScore = 0;
+
+let rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', function(e) {
+    game(e.target.textContent);
+});
+
+let paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', function(e) {
+    game(e.target.textContent);
+});
+
+let scissorBtn = document.querySelector('.scissor');
+scissorBtn.addEventListener('click', function(e) {
+    game(e.target.textContent);
+});
